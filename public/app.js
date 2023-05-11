bca4ecdc26224452b18a318d646690018d52a338
Zach Baskin <zbaskin@gatech.edu>
2023-05-11 12:22:56 -0400

    add signup/login functionality

--- public/app.js
+++ public/app.js
@@ -10,6 +10,59 @@ const firebaseConfig = {
 };
 firebase.initializeApp(firebaseConfig);
 
+// Signup form handling
+const signupForm = document.querySelector('#signup-form');
+signupForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const email = signupForm['email'].value;
+    const password = signupForm['password'].value;
+
+    // Create user with email and password
+    firebase.auth().createUserWithEmailAndPassword(email, password)
+    .then(() => {
+        // Redirect to movie list page or desired page
+        window.location.href = 'index.html';
+    })
+    .catch((error) => {
+        // Handle signup error
+        console.log(error);
+    });
+
+    // Clear form fields
+    signupForm.reset();
+});
+
+// Login form handling
+const loginForm = document.querySelector('#login-form');
+loginForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const email = loginForm['email'].value;
+    const password = loginForm['password'].value;
+
+    // Sign in with email and password
+    firebase.auth().signInWithEmailAndPassword(email, password)
+        .then(() => {
+        // Redirect to movie list page or desired page
+        window.location.href = 'movie-list.html';
+        })
+        .catch((error) => {
+        // Handle login error
+        console.log(error);
+        });
+
+    // Clear form fields
+    loginForm.reset();
+});
+
+// Redirect to the login page if the user is not authenticated
+firebase.auth().onAuthStateChanged((user) => {
+    if (!user) {
+        window.location.href = 'login.html';
+    }
+});
+
 // Handle form submission
 const addMovieForm = document.querySelector('#add-movie-form');
 addMovieForm.addEventListener('submit', (e) => {
