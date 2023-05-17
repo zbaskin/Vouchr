07df5840724c0b1b7755f804a28e0fb1d888ea7e
Zach Baskin <zbaskin@gatech.edu>
2023-05-17 19:55:44 -0400

    change firebase API key

--- public/login.js
+++ public/login.js
@@ -1,13 +1,5 @@
 // Initialize Firebase app
-const firebaseConfig = {
-    apiKey: "AIzaSyD3R2uh_DLnmkjQEhHo-cLj7RVANdFIw3w",
-    authDomain: "vouchr-9221a.firebaseapp.com",
-    projectId: "vouchr-9221a",
-    storageBucket: "vouchr-9221a.appspot.com",
-    messagingSenderId: "785980302686",
-    appId: "1:785980302686:web:bba9f2673d2574ab5e1bcf",
-    measurementId: "G-39Z1CQBWRL"
-};
+import { firebaseConfig } from './config/firebase.js';
 firebase.initializeApp(firebaseConfig);
 
 // Login form handling
