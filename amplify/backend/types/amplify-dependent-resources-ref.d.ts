80346a2d42818a287fb9bbc9b639a18f6e9ac76b
Zach Baskin <zbaskin@gatech.edu>
2024-02-04 19:31:35 -0500

    add amplify

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "vouchr": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  }
+}
