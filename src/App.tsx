90bd01e64bf168d0c6f1b5aa2ebdad4c0a694764
Zach Baskin <zbaskin@gatech.edu>
2025-09-16 20:23:50 -0400

    removed all commented out code

--- src/App.tsx
+++ src/App.tsx
@@ -72,7 +72,6 @@ const Settings: React.FC = () => (
 );
 
 const App: React.FC<AppProps> = ({ signOut, user }) => {
-  /*const [showForm, setShowForm] = useState(false);*/
   const [tickets, setTickets] = useState<Ticket[] | CreateTicketInput[]>([]);
   const [ticketCollection] = useState(user?.userId);
   const [isLoading, setIsLoading] = useState(false);
@@ -130,47 +129,9 @@ const App: React.FC<AppProps> = ({ signOut, user }) => {
     if (!ticketCollection) return;
     setIsLoading(true);
     const raw = await fetchTickets(ticketCollection);
-    /*
-    const sortType = await fetchSortType(ticketCollection);
-    if (sortType === SortType.ALPHABETICAL) {
-      fetchedTickets.sort((a, b) => {
-        return a.name.localeCompare(b.name);
-      });
-    }
-    if (sortType === SortType.TIME_CREATED) {
-      fetchedTickets.sort((a, b) => {
-        return b.timeCreated - a.timeCreated;
-      });
-    }
-    if (sortType === SortType.EVENT_DATE) {
-      fetchedTickets.sort((a, b) => {
-        const aDate = a.eventDate || "";
-        const bDate = b.eventDate || "";
-        const aTime = a.eventTime || "";
-        const bTime = b.eventTime || "";
-        const [aHour, aMinute] = aTime.substring(0, 5).split(':');
-        const [bHour, bMinute] = bTime.substring(0, 5).split(':');
-        const aDateTime = new Date(aDate);
-        const bDateTime = new Date(bDate);
-        aDateTime.setHours(parseInt(aHour));
-        aDateTime.setMinutes(parseInt(aMinute));
-        bDateTime.setHours(parseInt(bHour));
-        bDateTime.setMinutes(parseInt(bMinute));
-        return bDateTime.getTime() - aDateTime.getTime();
-      });
-    }
-    */
     setTickets(sortTickets(raw, sortType));
     setIsLoading(false);
   };
-
-  /*
-  const handleUpdateSort = async (collectionSortType: SortType) => {
-    if (!ticketCollection) return;
-    await updateSortType(ticketCollection, collectionSortType);
-    await handleFetchTickets();
-  }
-  */
 
   const handleAddTicket = async (newTicket: CreateTicketInput) => {
     if (!newTicket.name || !newTicket.ticketsID) return;
@@ -194,12 +155,6 @@ const App: React.FC<AppProps> = ({ signOut, user }) => {
       await addUser(user.userId, user.username as string, ticketCollection as string);
     }
   };
-
-  /*
-  const toggleFormDisplay = () => {
-    setShowForm(!showForm);
-  }
-  */
 
   return (
     <>
@@ -236,44 +191,6 @@ const App: React.FC<AppProps> = ({ signOut, user }) => {
         </Routes>
       </main>
     </>
-
-    
-    /*
-    <div className={"appContainer " + (isMobile ? "containerMobile" : "containerDesktop")}>
-      <Navbar isMobile={isMobile} />
-      <h2 className="title">{user?.username ? user.username.concat("'s") : (<h2>Your</h2>) } Ticket Collection</h2>
-      <div className="headerButtonContainer">
-        <button className="signoutButton" onClick={signOut}>Sign out</button>
-        <button className="createTicketButton" onClick={toggleFormDisplay}>
-          {!showForm ? <span>Create Ticket</span> : <span>Close</span>}
-        </button>
-      </div>
-      {showForm &&
-      <TicketForm
-        ticketCollection={ticketCollection}
-        onAddTicket={handleAddTicket}
-      />
-      }
-      <div className="sortButtonContainer">
-        <button className="sortButton" onClick={()=>handleUpdateSort(SortType.ALPHABETICAL)}>
-          Sort By Name
-        </button>
-        <button className="sortButton" onClick={()=>handleUpdateSort(SortType.TIME_CREATED)}>
-          Sort By Date Added
-        </button>
-        <button className="sortButton" onClick={()=>handleUpdateSort(SortType.EVENT_DATE)}>
-          Sort By Date Seen
-        </button>
-      </div>
-      <TicketCollection 
-        tickets={tickets as Ticket[]}
-        onRemoveTicket={handleRemoveTicket}
-        isLoading={isLoading}
-        isMobile={isMobile}
-      />
-      
-    </div>
-    */
   );
 };
 
